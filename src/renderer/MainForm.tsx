a905d600f5bdd2a9c55673f75535340685205b9f
hana <rlagksk8428@gmail.com>
2023-09-07 16:06:31 +0900

    feat: add dark theme, add upload components

--- src/renderer/MainForm.tsx
+++ src/renderer/MainForm.tsx
@@ -1,5 +1,6 @@
 import {
   AppBar,
+  Button,
   Container,
   CssBaseline,
   Grid,
@@ -8,6 +9,8 @@ import {
   Typography,
 } from '@mui/material';
 import { useState } from 'react';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import styled from '@emotion/styled';
 
 const defaultValues = {
   subj: '',
@@ -19,6 +22,19 @@ const defaultValues = {
   study_description: '',
   series_description: '',
 };
+
+const VisuallyHiddenInput = styled('input')`
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  white-space: nowrap;
+  width: 1px;
+  margin-bottom: 50px;
+`;
 
 function MainForm() {
   const [formValues, setFormValues] = useState(defaultValues);
@@ -65,14 +81,28 @@ function MainForm() {
       </AppBar>
       <Container
         component="main"
-        maxWidth="md"
-        sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+        sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 }, width: '50vw' }}
       >
-        <Typography component="h1" variant="h4" align="center">
+        <Typography
+          component="h1"
+          variant="h4"
+          align="center"
+          sx={{ 'margin-bottom': '150px' }}
+        >
           Checkout
         </Typography>
+
+        <Button
+          component="label"
+          variant="contained"
+          startIcon={<CloudUploadIcon />}
+          href="#file-up`load"
+        >
+          Upload dicom file
+          <VisuallyHiddenInput type="file" />
+        </Button>
         <form onSubmit={clickBtn}>
-          <Grid container spacing={3}>
+          <Grid container spacing={7}>
             <Grid item xs={12} sm={6}>
               <TextField
                 required
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,18 @@
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material';
 import App from './App';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
-root.render(<App />);
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+root.render(
+  <ThemeProvider theme={darkTheme}>
+    <App />
+  </ThemeProvider>
+);
