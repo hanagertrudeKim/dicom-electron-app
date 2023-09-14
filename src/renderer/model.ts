b171fea9310de70e3475e26b639dddd2710597ea
hana <rlagksk8428@gmail.com>
2023-09-14 18:50:09 +0900

    fix: fix form value

--- src/renderer/model.ts
+++ src/renderer/model.ts
@@ -1,11 +1,4 @@
 export default interface defaultValues {
   subj: string;
-  patient_id: number;
-  patient_name: string;
-  ct_date: number;
-  slice_thickness: number;
-  number_of_slices: number;
-  study_description: string;
-  series_description: string;
-  dicom_file: any;
+  dicom: FileList | null | undefined;
 }
